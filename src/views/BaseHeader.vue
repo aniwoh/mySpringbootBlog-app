af0a856a1483e02299877ff7c20267432e999ec1
aniwoh <aniwoh@126.com>
2024-08-02 07:48:33 +0000

    00

--- src/views/BaseHeader.vue
+++ src/views/BaseHeader.vue
@@ -2,26 +2,26 @@
   <el-header class="me-area" :class="{'header-hidden': isHidden}">
     <el-row class="me-header" :gutter="20">
       <el-col :span="4" class="me-header-left">
-        <router-link to="/" class="me-title">
+        <router-link to="/" class="me-title" @click="handleTagClick(-1)">
           <img alt="" src="/avater_128.ico">
         </router-link>
       </el-col>
       <el-col :span="14">
         <!-- mode="horizontal" 为水平模式-->
-        <el-menu :router=true :default-active="activeIndex" active-text-color="#5FB878" mode="horizontal">
-          <el-menu-item index="/">首页</el-menu-item>
-          <el-sub-menu index="">
+        <el-menu :default-active="activeIndex" active-text-color="#5FB878" mode="horizontal" >
+          <el-menu-item index="1" route="/" @click="handleTagClick(-1)"><router-link to="/">首页</router-link></el-menu-item>
+          <el-sub-menu index="2">
             <template #title>分类</template>
-            <el-menu-item index="" @click="handleTagClick(-1)">默认排序</el-menu-item>
-            <el-menu-item index="" @click="handleTagClick(-2)">按点赞数排序</el-menu-item>
-            <el-menu-item index="" @click="handleTagClick(-3)">按浏览量排序</el-menu-item>
-            <el-menu-item index="" @click="handleTagClick(-4)">按评论数排序</el-menu-item>
-            <el-sub-menu index="4-1">
+            <el-menu-item index="2-1" @click="handleTagClick(-1)"><router-link to="/">默认排序</router-link></el-menu-item>
+            <el-menu-item index="2-2" @click="handleTagClick(-2)"><router-link to="/">按点赞数排序</router-link></el-menu-item>
+            <el-menu-item index="2-3" @click="handleTagClick(-3)"><router-link to="/">按浏览量排序</router-link></el-menu-item>
+            <el-menu-item index="2-4" @click="handleTagClick(-4)"><router-link to="/">按评论数排序</router-link></el-menu-item>
+            <el-sub-menu index="">
               <template #title>标签筛选</template>
-              <el-menu-item v-for="tag in tags"  @click="handleTagClick(tag.id)">{{tag.tagName}}</el-menu-item>
+              <el-menu-item v-for="tag in tags"  @click="handleTagClick(tag.id)"><router-link to="/">{{tag.tagName}}</router-link></el-menu-item>
             </el-sub-menu>
           </el-sub-menu>
-          <el-menu-item index="">归档</el-menu-item>
+          <el-menu-item index="3">归档</el-menu-item>
           <el-col :span="4" style="padding: 10px">
             <el-input  size="large" placeholder="搜索文章" v-model="keywords"></el-input>
           </el-col>
@@ -74,6 +74,9 @@ const search = ()=>{
   prop.article_list.value= prop.article_list.value.filter(item =>
       item.title.toLowerCase().includes(keywords.value.toLowerCase())
   );
+}
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath)
 }
 </script>
 
