82cae42045ff69c802558fb3759f7bda544855a1
aniwoh <104836158+aniwoh@users.noreply.github.com>
2024-11-19 21:43:23 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+export default defineConfig(({ mode }) => {
+  // 检查当前环境变量，决定是否启用 /api 代理
+  const isProxyEnabled = mode === 'development'; // 开发环境启用 /api 代理，生产环境禁用
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/', // 设置基础路径
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    port: '5173',
-    host: '::', // 允许外部访问
-    proxy:{
-      '/api':{
-        target:'http://localhost:8888/',
-        changeOrigin:true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+  return {
+    base: '/', // 基础路径
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-      '/randomPic':{
-        target: 'https://www.dmoe.cc/random.php?return=json',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/randomPic/, '')
-      }
-    }
-  }
-})
+    },
+    server: {
+      port: '5173',
+      host: '::', // 允许外部访问
+      proxy: {
+        ...(isProxyEnabled && { // 仅在开发环境启用 /api 代理
+          '/api': {
+            target: 'http://localhost:8888/',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        }),
+        '/randomPic': {
+          target: 'https://www.dmoe.cc/random.php?return=json',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/randomPic/, ''),
+        },
+      },
+    },
+  };
+});
